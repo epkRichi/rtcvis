53c99a9e31130fcc4f08d60d75c5530d6fd98e16
epkRichi <75546561+epkRichi@users.noreply.github.com>
2025-03-23 16:43:05 +0100

    Added more tests for PLF match

--- tests/test_plf_match.py
+++ tests/test_plf_match.py
@@ -13,3 +13,15 @@ def test_2():
     new_a, new_b = PLF.match(a, b)
     assert new_a == a
     assert new_b == PLF([Point(0, 1), Point(1, 2), Point(1, 2), Point(2, 3)])
+
+def test_3():
+    a = PLF([Point(0, 1), Point(4, 5), Point(8, 4)])
+    b = PLF([Point(0, 2), Point(2, 0), Point(5, 1), Point(8, -2)])
+    new_a, new_b = PLF.match(a, b)
+    assert new_a == PLF([Point(0, 1), Point(2, 3), Point(4, 5), Point(5, 4.75), Point(8, 4)])
+    assert new_b == PLF([Point(0, 2), Point(2, 0), Point(4, 2/3), Point(5, 1), Point(8, -2)])
+
+def test_4():
+    a = PLF([Point(0, 2), Point(2, 0), Point(5, 1), Point(8, -2)])
+    new_a_1, new_a_2 = PLF.match(a, a)
+    assert new_a_1 == new_a_2
