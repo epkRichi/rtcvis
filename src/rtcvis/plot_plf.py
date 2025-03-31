1ebaa499d5450ce015624747704465cd79e54f33
epkRichi <75546561+epkRichi@users.noreply.github.com>
2025-03-31 17:02:34 +0200

    Fixed the PLF plot function

--- src/rtcvis/plot_plf.py
+++ src/rtcvis/plot_plf.py
@@ -4,69 +4,67 @@ import numpy as np
 
 from rtcvis.plf import PLF
 from rtcvis.point import Point
-from rtcvis.conv import ConvType, conv, min_plus_conv
+from rtcvis.conv import ConvType, conv, conv_at_x
 
 
-def plot_plf(a: PLF, b: PLF):
+def plot_conv(a: PLF, b: PLF):
+    assert a.x_start == 0 and b.x_start == 0 and a.x_end == b.x_end
     fig, ax = plt.subplots()
     ax.set_aspect("equal", adjustable="box")
+
+    # compute initial convolution result
+    initial_x = 0
+    conv_type = ConvType.MIN_PLUS_CONV
+    conv_result = conv_at_x(a, b, initial_x, conv_type)
 
     # Make room for bottom slider
     fig.subplots_adjust(bottom=0.25)
 
     # Create bottom slider
-    axdeltax = fig.add_axes([0.25, 0.1, 0.65, 0.03])
-    deltax_slider = Slider(ax=axdeltax, label="delta_x", valmin=0, valmax=10, valinit=0)
+    axdeltax = fig.add_axes((0.25, 0.1, 0.65, 0.03))
+    deltax_slider = Slider(
+        ax=axdeltax, label="delta_x", valmin=0, valmax=a.x_end, valinit=initial_x
+    )
 
-    # plot a
-    x = [p.x for p in a.points]
-    y = [p.y for p in a.points]
-    ax.plot(x, y)
+    # plot transformed a
+    (graph_a,) = ax.plot(conv_result.transformed_a.x, conv_result.transformed_a.y)
 
     # plot b
-    b = b.transformed(True, 0)
-    x = [p.x for p in b.points]
-    y = [p.y for p in b.points]
-    (graph_b,) = ax.plot(x, y)
+    ax.plot(b.x, b.y)
 
-    # plot conv at x
-    initial_x = 0
-    s, res = min_plus_conv(a, b, delta_x=initial_x)
-    x = [p.x for p in s.points]
-    y = [p.y for p in s.points]
-    (graph_sum,) = ax.plot(x, y)
+    # plot convolution sum
+    (graph_sum,) = ax.plot(conv_result.sum.x, conv_result.sum.y)
 
     # add marker for conv result
     # (graph_marker,) = ax.plot([initial_x], res, marker="s")
 
     # Slider update function
     def update(val):
-        # Update sum function
-        s, res = min_plus_conv(a, b, delta_x=val)
-        x = [p.x for p in s.points]
-        y = [p.y for p in s.points]
-        graph_sum.set_xdata(x)
-        graph_sum.set_ydata(y)
+        # Recompute convolution
+        conv_result = conv_at_x(a, b, val, conv_type)
+
+        # Update transformed a
+        graph_a.set_xdata(conv_result.transformed_a.x)
+        graph_a.set_ydata(
+            conv_result.transformed_a.y
+        )  # y doesn't really change but hey
+        fig.canvas.draw_idle()
+
+        # Update sum PLF
+        graph_sum.set_xdata(conv_result.sum.x)
+        graph_sum.set_ydata(conv_result.sum.y)
 
         # # Update minconv marker
         # graph_marker.set_xdata([s.min])
         # graph_marker.set_ydata([min_y])
 
-        # Update function b
-        b = b.transformed(False, val)
-        x = [p.x for p in b.points]
-        graph_b.set_xdata(x)
-        fig.canvas.draw_idle()
-
     # register the slider
     deltax_slider.on_changed(update)
 
-    return fig
+    plt.show()
 
 
 if __name__ == "__main__":
-    a = PLF([Point(0, 0), (0.1, 0.0), (0.5, 0.4)])
+    a = PLF([Point(0, 0), (1, 0), (5, 4)])
     b = PLF([(0, 0), (1, 0), (2, 1), (3, 1), (4, 2), (5, 2)])
-    c = conv(a, b, ConvType.MIN_PLUS_CONV)
-    fig = plot_plf(a, b)
-    plt.show()
+    plot_conv(a, b)
