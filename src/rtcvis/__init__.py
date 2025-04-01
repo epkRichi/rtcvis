70c2b0c5655b11408357861fbeb05256eb895014
epkRichi <75546561+epkRichi@users.noreply.github.com>
2025-04-01 14:13:39 +0200

    Added mypy to pre-commit hooks

--- src/rtcvis/__init__.py
+++ src/rtcvis/__init__.py
@@ -3,4 +3,4 @@ from rtcvis.plf import PLF
 from rtcvis.conv import ConvType, conv, conv_at_x
 from rtcvis.plot_conv import plot_conv
 
-__all__ = (Point, PLF, ConvType, conv, conv_at_x, plot_conv)
+__all__ = ("Point", "PLF", "ConvType", "conv", "conv_at_x", "plot_conv")
