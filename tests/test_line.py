1d9275bc7a63e6da99713295e3a3e500beea6206
epkRichi <75546561+epkRichi@users.noreply.github.com>
2025-04-08 17:46:47 +0200

    Refactored test_line.py

--- tests/test_line.py
+++ tests/test_line.py
@@ -4,62 +4,61 @@ from rtcvis.point import Point
 from rtcvis.line import Line, line_intersection
 
 
-def test_init():
-    # Check that init works with both Points and Tuples
-    Line((0, 0), (1, 0))
-    Line((0, 0), Point(1, 0))
-    Line(Point(0, 0), (1, 0))
-    Line(Point(0, 0), Point(1, 0))
-    # Check the properties of a "normal" line
-    line = Line((0, 0), (1, 1))
-    assert line.a == Point(0, 0)
-    assert line.b == Point(1, 1)
-    assert line.slope == 1
-    assert not line.is_vertical
-    # Check the properties of a line with slope 0 and swapped points
-    line = Line((1, 0), (0, 0))
-    assert line.a == Point(0, 0)
-    assert line.b == Point(1, 0)
-    assert line.slope == 0
-    assert not line.is_vertical
-    # Check that Vertical lines can be created
-    line = Line((0, 0), (0, 1))
-    assert line.a == Point(0, 0)
-    assert line.b == Point(0, 1)
-    assert line.slope is None
-    assert line.is_vertical
+@pytest.mark.parametrize(
+    "a,b,e_a,e_b,e_is_vertical,e_slope",
+    [
+        # Check that init works with both Points and Tuples
+        ((0, 0), (1, 0), (0, 0), (1, 0), False, 0),
+        ((0, 0), Point(1, 0), (0, 0), (1, 0), False, 0),
+        (Point(0, 0), (1, 0), (0, 0), (1, 0), False, 0),
+        (Point(0, 0), Point(1, 0), (0, 0), (1, 0), False, 0),
+        # a slope that's not 0
+        ((0, 0), (1, 1), (0, 0), (1, 1), False, 1),
+        # Swapped points
+        ((1, 0), (0, 0), (0, 0), (1, 0), False, 0),
+        # vertical line
+        ((0, 0), (0, 1), (0, 0), (0, 1), True, None),
+    ],
+)
+def test_init(a, b, e_a, e_b, e_is_vertical, e_slope):
+    # construct the line
+    line = Line(a, b)
+    assert line.a == Point(*e_a)
+    assert line.b == Point(*e_b)
+    assert line.is_vertical == e_is_vertical
+    assert line.slope == e_slope
+
+
+def test_init_invalid():
     # Check that lines can't be created from identical Points
     with pytest.raises(AssertionError):
         Line((3, 3), (3, 3))
 
 
-def test_line_intersection():
-    # normal line intersections
-    i = line_intersection(Line((0, 0), (1, 1)), Line((0, 1), (1, 0)))
-    assert i == (0.5, 0.5)
-    i = line_intersection(Line((0, 0), (1, 1)), Line((1, 1), (2, 0)))
-    assert i == (1, 1)
-    i = line_intersection(Line((-1, -1), (1, 0)), Line((2, 4), (4, -2)))
-    assert i == (3, 1)
-    # first and second point swapped
-    i = line_intersection(Line((1, 0), (-1, -1)), Line((2, 4), (4, -2)))
-    assert i == (3, 1)
-    # parallel lines, both identical and different
-    i = line_intersection(Line((0, 0), (1, 0)), Line((5, 6), (3, 6)))
-    assert i is None
-    i = line_intersection(Line((0, 0), (1, 0)), Line((0, 0), (1, 0)))
-    assert i is None
-    i = line_intersection(Line((0, 0), (1, 0)), Line((-1, 0), (3, 0)))
-    assert i is None
-    # two vertical lines, both identical and different
-    i = line_intersection(Line((0, 0), (0, -5)), Line((0, 0), (0, -5)))
-    assert i is None
-    i = line_intersection(Line((0, 0), (0, -5)), Line((0, -1), (0, 3)))
-    assert i is None
-    i = line_intersection(Line((0, 0), (0, -5)), Line((2, -1), (2, 3)))
-    assert i is None
-    # one vertical line, one non-vertical line
-    i = line_intersection(Line((0, 0), (0, -5)), Line((-1, 0), (1, 1)))
-    assert i == (0, 0.5)
-    i = line_intersection(Line((0, 0), (0, -5)), Line((-1, 0), (1, 0)))
-    assert i == (0, 0)
+@pytest.mark.parametrize(
+    "a,b,expected",
+    [
+        # normal line intersections
+        (((0, 0), (1, 1)), ((0, 1), (1, 0)), (0.5, 0.5)),
+        (((0, 0), (1, 1)), ((1, 1), (2, 0)), (1, 1)),
+        (((-1, -1), (1, 0)), ((2, 4), (4, -2)), (3, 1)),
+        # first and second point swapped
+        (((1, 0), (-1, -1)), ((2, 4), (4, -2)), (3, 1)),
+        # parallel lines, both identical and different
+        (((0, 0), (1, 0)), ((5, 6), (3, 6)), None),
+        (((0, 0), (1, 0)), ((0, 0), (1, 0)), None),
+        (((0, 0), (1, 0)), ((-1, 0), (3, 0)), None),
+        # two vertical lines, both identical and different
+        (((0, 0), (0, -5)), ((0, 0), (0, -5)), None),
+        (((0, 0), (0, -5)), ((0, -1), (0, 3)), None),
+        (((0, 0), (0, -5)), ((2, -1), (2, 3)), None),
+        # one vertical line, one non-vertical line
+        (((0, 0), (0, -5)), ((-1, 0), (1, 1)), (0, 0.5)),
+        (((0, 0), (0, -5)), ((-1, 0), (1, 0)), (0, 0)),
+    ],
+)
+def test_line_intersection(a, b, expected):
+    line_a = Line(*a)
+    line_b = Line(*b)
+    result = line_intersection(line_a, line_b)
+    assert result == expected
