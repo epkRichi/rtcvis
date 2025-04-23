afde7eab592614b04e6569459d66356d9ad4526f
epkRichi <75546561+epkRichi@users.noreply.github.com>
2025-04-23 21:27:17 +0200

    Removed RTC output functions
    It is still part of another branch

--- src/rtcvis/__init__.py
+++ src/rtcvis/__init__.py
@@ -3,12 +3,6 @@ from rtcvis.plf import PLF
 from rtcvis.conv import ConvType, conv, conv_at_x
 from rtcvis.plot_conv import plot_conv
 from rtcvis.plot_plf import plot_plfs
-from rtcvis.rtc import (
-    lower_service_out,
-    upper_service_out,
-    upper_events_out,
-    lower_events_out,
-)
 
 __all__ = (
     "Point",
@@ -17,9 +11,5 @@ __all__ = (
     "conv",
     "conv_at_x",
     "plot_conv",
-    "lower_service_out",
-    "upper_service_out",
-    "upper_events_out",
-    "lower_events_out",
     "plot_plfs",
 )
