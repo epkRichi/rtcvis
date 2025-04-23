5dd9ca69185b8715f88b8608618e108204304084
epkRichi <75546561+epkRichi@users.noreply.github.com>
2025-04-23 17:33:33 +0200

    Added a test that for RTCOut that fails.
    This test will keep failing unless some very deep changes are take place. There is a note about it in src/rtcvis/rtc.py.

--- src/rtcvis/rtc.py
+++ src/rtcvis/rtc.py
@@ -2,6 +2,67 @@ from typing import Sequence
 from rtcvis.plf import PLF, plf_min_max
 from rtcvis.conv import ConvType, conv
 
+"""
+The current approach for PLFs and convolutions is too simple to compute RTC output
+curves. The key problem is that event and service curves are assumed to be repeating
+periodically, and this has to be respected when computing convolutions. The current
+function for computing convolution can only handle finite PLFs.
+
+For deconvolutions, PLF a simply has to be moved to the left. Handling periodic
+functions should not be a huge problem since both the transformed a and b are always
+defined for all x>=0. Each period of b will always see the same part of a, just with a
+different y-offset. This means that we can simply repeat a once and compute the
+convolution the way it is done right now and the result just has to be interpreted as a
+periodically repeating function as well. The only thing to consider is that for example
+for the min-plus-deconvolution, the maximum of the difference between a and b is
+computed. If now the y-offset/the difference keeps getting bigger and bigger, the result
+would be infinity. We could either introduce a way to represent infinity (technically
+this is already possible with floats...) or we could throw an error. If the differnce
+does not get bigger, then it gets smaller or stays the same, meaning that the first
+period will be where the maximum difference is located and that's why the current
+implementation works for many PLFs. For the max-plus-deconvolution, we instead have to
+check that the difference (a-b) keeps getting bigger since we're computing the minimum.
+
+Convolutions are unfortunately more tricky. Since we mirror PLF a before offsetting it
+on the x-axis, this means that the area of overlap between a and b is finite (and keeps
+getting bigger). So when computing convolutions, we'll also have many periods that
+fully overlap with a and which will thus all see the same values, just at different y
+offsets. The problem is however that the period where the current "end" of a is will
+only partially overlap with a and it can thus have a different result (even when
+considering the different y-offset) than the other "repetitive" periods. What will
+happen is that the extremum we're looking for might be in the non-overlapping period at
+first but at some x, it can change and the extremum will then be located in the
+overlapping periods. I'm not sure when exactly this switch will happen or how to compute
+that. But it will lead to a resulting PLF that starts with an aperiodic part and which
+continues with a periodic part. I'm also not sure if the periodic and aperiodic parts
+will always have the same length as the periods of a and b or if that might differ.
+
+So, if we really want to compute RTC output curves, we have to support PLFs with an
+aperiodic part at the start, followed by a periodic part. Supporting that in all
+existing methods like add and match will be a lot of work and after that, the
+convolution function still has to be adapted and I'm not sure how complicated this is
+going to be. For this reason, I've decided to remove all output-curve related code from
+main and put it on a separate branch.
+
+I'm however not sure if I'll even try to add the described features to rtcvis. I've
+mainly created this package for visualizing (de-)convolutions with the min-plus-algebra
+and this works very well. Computing output functions would be very cool of course, but
+it would hardly add anything that the rtctoolbox from ETH Zürich can't do. Using
+rtctoolbox, you can also plot every individual step of computing the output curves. The
+only thing it can't do is give you an interactive plot for computing the convolutions
+of curves (and since these have to be periodic, rtcvis can't do that right now either).
+So adding support for periodic functions sounds like a lot of work for very few benefits
+(unless we count using matlab as a major disadvantage for rtctoolbox :D)
+
+Anyways, here are two PLFs where if you interpret them as periodic PLFs (without
+aperiodic parts), the current convolution function computes an incorrect result for the
+min plus deconv:
+a = PLF([(0, 0), (9, 9), (15, 9)])
+b = PLF([(0, 0), (2, 0), (15, 13)])
+The part with slope 0 at the start has to be longer than the following
+parts of slope 0, which are all the same length.
+"""
+
 
 def check_plf_rtc(plfs: Sequence[PLF]) -> bool:
     """Check whether the PLFs are valid for RTC operations.
--- tests/test_rtc_out.py
+++ tests/test_rtc_out.py
@@ -3,7 +3,7 @@ from rtcvis import *
 
 
 @pytest.mark.parametrize(
-    "upper_service_in,lower_service_in,upper_events_in,lower_events_in,expected_upper_service_out,expected_lower_service_out",  # noqa: E501
+    "upper_service_in,lower_service_in,upper_events_in,lower_events_in,expected_upper_service_out,expected_lower_service_out,expected_upper_events_out,expected_lower_events_out",  # noqa: E501
     [
         # (
         #     [(0, 0), (30, 30)],
@@ -57,6 +57,53 @@ from rtcvis import *
             ),
             PLF([(0, 0), (4, 4), (15, 4)]),
             PLF([(0, 0), (11, 0), (15, 4)]),
+            PLF(
+                [
+                    # (0, 0),
+                    (0, 1),
+                    (1, 1),
+                    (1, 2),
+                    (2, 2),
+                    (2, 3),
+                    (3, 3),
+                    (3, 4),
+                    (4, 4),
+                    (4, 5),
+                    (5, 5),
+                    (5, 6),
+                    (6, 6),
+                    (6, 7),
+                    (7, 7),
+                    (7, 8),
+                    (8, 8),
+                    (8, 9),
+                    (13, 9),
+                    (13, 10),
+                    (14, 10),
+                    (14, 11),
+                    (15, 11),
+                    (15, 12),
+                ]
+            ),
+            PLF(
+                [
+                    # (0, 0),
+                    (9, 0),
+                    (9, 1),
+                    (10, 1),
+                    (10, 2),
+                    (11, 2),
+                    (11, 3),
+                    (12, 3),
+                    (12, 4),
+                    (13, 4),
+                    (13, 5),
+                    (14, 5),
+                    (14, 6),
+                    (15, 6),
+                    (15, 7),
+                ]
+            ),
         )
     ],
 )
@@ -69,6 +116,8 @@ class TestRTCOut:
         lower_events_in,
         expected_upper_service_out,
         expected_lower_service_out,
+        expected_upper_events_out,
+        expected_lower_events_out,
     ):
         result = upper_service_out(
             upper_service_in=upper_service_in, lower_events_in=lower_events_in
@@ -83,8 +132,46 @@ class TestRTCOut:
         lower_events_in,
         expected_upper_service_out,
         expected_lower_service_out,
+        expected_upper_events_out,
+        expected_lower_events_out,
     ):
         result = lower_service_out(
             lower_service_in=lower_service_in, upper_events_in=upper_events_in
         )
         assert result == expected_lower_service_out
+
+    def test_upper_events_out(
+        self,
+        upper_service_in,
+        lower_service_in,
+        upper_events_in,
+        lower_events_in,
+        expected_upper_service_out,
+        expected_lower_service_out,
+        expected_upper_events_out,
+        expected_lower_events_out,
+    ):
+        result = upper_events_out(
+            upper_events_in=upper_events_in,
+            lower_service_in=lower_service_in,
+            upper_service_in=upper_service_in,
+        )
+        assert result == expected_upper_events_out
+
+    def test_lower_events_out(
+        self,
+        upper_service_in,
+        lower_service_in,
+        upper_events_in,
+        lower_events_in,
+        expected_upper_service_out,
+        expected_lower_service_out,
+        expected_upper_events_out,
+        expected_lower_events_out,
+    ):
+        result = lower_events_out(
+            lower_events_in=lower_events_in,
+            lower_service_in=lower_service_in,
+            upper_service_in=upper_service_in,
+        )
+        assert result == expected_lower_events_out
