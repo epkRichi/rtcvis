2d5612071c40a6a8f8b74494ce9c87a471841710
epkRichi <75546561+epkRichi@users.noreply.github.com>
2025-04-23 21:41:20 +0200

    Slightly changed example.py

--- example.py
+++ example.py
@@ -4,6 +4,8 @@ from rtcvis import *
 if __name__ == "__main__":
     a = PLF([Point(0, 0), (1, 0), (5, 4)])
     b = PLF([(0, 0), (1, 0), (2, 1), (3, 1), (4, 2), (5, 2)])
+    plot_plfs([a, b])
+
     plot_conv(a, b, ConvType.MIN_PLUS_CONV, True)
     plot_conv(a, b, ConvType.MIN_PLUS_DECONV, True)
     plot_conv(a, b, ConvType.MAX_PLUS_CONV, True)
